64a4b1037cca4f782dc193fbdd6c6a1cd4796021
Stephen Nixon <swr410@gmail.com>
2018-03-03 17:42:59 +0100

    first commit

---
+++ add-grid-of-guides.py
@@ -0,0 +1,42 @@
+# purpose: place a grid of guides on all glyphs
+# stretch: let users dictate grid density either by UPM divisions or by distance, starting from origin
+
+f = CurrentFont()
+g = CurrentGlyph()
+UPM = f.info.unitsPerEm
+
+
+## Set grid size or number of divisions in UPM2
+# gridSize = 25
+divisions = 20
+gridSize = int(round(UPM / divisions))
+
+
+# clear existing guides in glyph
+def clearGuides():
+    for guide in g.guides:
+        g.removeGuide(guide)
+
+clearGuides()
+
+# add vertical guides
+for x in range(0, g.width+gridSize, gridSize):
+    g.addGuide((x, 0),90)
+    
+# add horizontal guides
+for y in range(0, UPM, gridSize):
+    startAt = f.info.descender
+    startAt += y
+    g.addGuide((0, startAt),0)
+    
+# make guides magnetic
+for guide in g.guides:
+    # prevent vertical guides from being far more "magnetic"
+    if guide.angle == 90:
+        guide.magnetic = 10
+    else:
+        guide.magnetic = 10
+    
+    
+    
+    
---
+++ highlight-points-on-guidelines.py
@@ -0,0 +1,10 @@
+## a tool to place diamond shapes on points when they touch a guideline (possibly with a different color when they touch a metric guideline or global guideline), similar to how glyphs shows this
+
+# make a list of guidelines
+# make a list of metric guidelines
+
+# for each point
+    # if y is in the list of guidelines 
+        # place a faint brown rectangle at this position
+        # rotate by 45 degrees
+        # move down and left by half of its size
